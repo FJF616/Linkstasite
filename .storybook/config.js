ee0917a2d01c4d34f0ec3d6ecb0f05f7b7a77da3
FJF616 <franzjfernando@hotmail.com>
2018-06-29 19:33:29 -0700

    added storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
 import { configure } from '@storybook/react';
-
+import { withBackgrounds } from '@storybook/addon-backgrounds';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 const req = require.context('../src/components', true, /\.stories\.js$/)
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
 
+stories.addDecorator(withKnobs);
+
+addDecorator(
+  withBackgrounds([
+    { name: 'twitter', value: '#00aced', default: true },
+    { name: 'facebook', value: '#3b5998' },
+  ])
+);
 configure(loadStories, module);
