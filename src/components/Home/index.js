91ae54c54a91673ee65baf4d4d9a9a13d7d882a0
FJF616 <franzjfernando@hotmail.com>
2018-07-04 01:00:55 -0700

    corrects syntax error

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
-import { compose } from 'recompose';
+
 // import{ BrowserRouter } from 'react-router-dom';
 // import Header from '../Header/Header';
 
